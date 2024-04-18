a189ef3025fbb3826cbf1946f38b2571f8a5c6f2
fouzaan <fouzaanahmed1@gmail.com>
2024-04-17 20:55:18 -0400

    Final commit 3

--- server/config/db.ts
+++ server/config/db.ts
@@ -4,7 +4,7 @@ if(LOCAL){
     URI = "mongodb://localhost/contacts";
     HostName = "localhost";
 }else{
-    URI = "";
+    URI = "mongodb+srv://fouzaanshaiknissar:5y43WALi84nXWIbq@contacts.2mvrmhn.mongodb.net/?retryWrites=true&w=majority&appName=Contacts";
     HostName = "MongoDB Atlas";
 }
 
